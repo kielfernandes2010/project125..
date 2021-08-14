e6f897cedfe53faf1766c85db273e1ada591982c
kielfernandes2010 <74675067+kielfernandes2010@users.noreply.github.com>
2021-08-14 13:56:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(450, 500);
+    
+
+    canvas=createCanvas(500,500);
+    canvas.position(700, 150);
+    canvas.center();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#969A97');
+    textSize(10);
+    fill('#F90093');
+    textSize('Kiel', 50, 400);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        
+    }
+}
+
+
